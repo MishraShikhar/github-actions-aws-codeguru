d304082fcb95fca78070a6245d2bd08b9497dec6
Shikhar Mishra <shikhami@amazon.com>
2022-06-15 21:44:02 -0400

    Costants file updated

--- spring-boot-hello-world-example/src/main/java/com/helloworld/Constants.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/Constants.java
@@ -1,11 +1,33 @@
 package com.helloworld;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 public class Constants {
     // private constructor to avoid instantiation
     private Constants(){}
     
-    // constants for product codes
+    private static String ACCESS_KEY = "";
+    private static String SECERET_KEY = "";
     public static final String PROD_CODE = "010";
-    public static final String DEV_CODE = "011";
-    public static final String TEST_CODE = "012";
-    public static final String QA_CODE = "013";
+    private static Constants Constants = null;
+    static AWSCredentials ac;
+    static AmazonDynamoDB dynamodb;
+    public synchronized static Constants getInstance() {
+        if (Constants == null) {
+            Constants = new Constants();
+
+            ac = new BasicAWSCredentials(ACCESS_KEY, SECERET_KEY);
+
+            dynamodb = AmazonDynamoDBClientBuilder
+                    .standard()
+            .withRegion("us-west-2")
+            .withCredentials(new AWSStaticCredentialsProvider(ac) )
+            .build();
+            System.out.println("Repo instance created");
+        }
+        return Constants;
+    }
 }
