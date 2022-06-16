48f04af57b3c2ee93cd9ba0b80aceb0aeb119005
Shikhar Mishra <shikhami@amazon.com>
2022-06-15 21:13:53 -0400

    Costants file added

---
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/Constants.java
@@ -0,0 +1,11 @@
+package com.helloworld;
+public class Constants {
+    // private constructor to avoid instantiation
+    private Constants(){}
+    
+    // constants for product codes
+    public static final String PROD_CODE = "010";
+    public static final String DEV_CODE = "011";
+    public static final String TEST_CODE = "012";
+    public static final String QA_CODE = "013";
+}
